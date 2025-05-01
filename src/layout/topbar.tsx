ae13ac92818d926a672de32780d33604df990756
Kim-soung-won <rlatmddnjs0103@naver.com>
2025-05-01 17:14:23 +0900

    create next app

---
+++ src/layout/topbar.tsx
@@ -0,0 +1,23 @@
+import { AppBar, Toolbar, Typography, IconButton, Button } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+
+
+export function TopMenuBar() {
+  return (
+    <AppBar position="sticky" color="default" elevation={1}>
+      <Toolbar>
+        {/* 좌측: 메뉴 아이콘 */}
+        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+          <MenuIcon />
+        </IconButton>
+        {/* 서비스명 또는 로고 */}
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          MyService
+        </Typography>
+        {/* 우측: 로그인/회원가입 등 버튼 */}
+        <Button color="inherit">로그인</Button>
+        <Button color="inherit">회원가입</Button>
+      </Toolbar>
+    </AppBar>
+  );
+}
